dfbea757f16a5c42553f68f6d2884d7f66d21d48
ppedrycz <piotrekpedrycz@gmail.com>
2017-04-23 20:55:58 +0200

    Switched to jpa converter

--- src/main/java/cz/pedry/nohibernate/utils/NoHibernateUtils.java
+++ src/main/java/cz/pedry/nohibernate/utils/NoHibernateUtils.java
@@ -1,37 +1,51 @@
 package cz.pedry.nohibernate.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import cz.pedry.nohibernate.NoHibernate;
 
+import java.io.IOException;
+
 public class NoHibernateUtils {
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     public NoHibernateUtils() { }
 
-    public static String object2CustomString(Object o, ObjectMapper objectMapper) {
-        try {
-            String javaClass = o.getClass().getName();
-            ObjectNode javaData = objectMapper.convertValue(o, ObjectNode.class);
-            ObjectNode objectNode = objectMapper.createObjectNode();
-            objectNode.put(NoHibernate.FIELD_CLASS, javaClass);
-            objectNode.set(NoHibernate.FIELD_DATA, javaData);
-            return objectMapper.writeValueAsString(objectNode);
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static String object2CustomString(Object o) {
+        if (o == null) {
+            throw new IllegalArgumentException("Object cannot be null");
         }
-        return null;
+        String javaClass = o.getClass().getName();
+        ObjectNode javaData = OBJECT_MAPPER.convertValue(o, ObjectNode.class);
+        ObjectNode objectNode = OBJECT_MAPPER.createObjectNode();
+        objectNode.put(NoHibernate.FIELD_CLASS, javaClass);
+        objectNode.set(NoHibernate.FIELD_DATA, javaData);
+        try {
+            return OBJECT_MAPPER.writeValueAsString(objectNode);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException("Object \"" + o + "\" cannot be translated to string");
+        }
     }
 
-    public static Object customString2Object(String s, ObjectMapper objectMapper) {
+    public static Object customString2Object(String s) {
+        String javaClass = null;
         try {
-            JsonNode jsonNode = objectMapper.readTree(s);
-            String javaClass = jsonNode.get(NoHibernate.FIELD_CLASS).textValue();
-            return objectMapper.treeToValue(jsonNode.get(NoHibernate.FIELD_DATA), Class.forName(javaClass));
-        } catch (Exception e) {
-            e.printStackTrace();
+            JsonNode jsonNode = OBJECT_MAPPER.readTree(s);
+            javaClass = jsonNode.get(NoHibernate.FIELD_CLASS).textValue();
+            if (javaClass == null || javaClass.equals("")) {
+                throw new IllegalArgumentException("String \"" + s + "\" has no \"" +
+                        NoHibernate.FIELD_CLASS + "\" parameter");
+            }
+            return OBJECT_MAPPER.treeToValue(jsonNode.get(NoHibernate.FIELD_DATA), Class.forName(javaClass));
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Provided string \"" + s + "\" is not in JSON format");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalArgumentException("Provided \"" + NoHibernate.FIELD_CLASS + "\" parameter \"" +
+                    javaClass + "\" cannot be found on classpath");
         }
-        return null;
     }
 
 }
--- src/test/java/cz/pedry/nohibernate/SerializationTest.java
+++ src/test/java/cz/pedry/nohibernate/SerializationTest.java
@@ -1,6 +1,5 @@
 package cz.pedry.nohibernate;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import cz.pedry.nohibernate.model.Circle;
 import cz.pedry.nohibernate.utils.NoHibernateUtils;
 import org.junit.Assert;
@@ -10,20 +9,18 @@ public class SerializationTest {
 
     @Test
     public void simplePojoShouldBeSerializedProperly() throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
         Object circleObject = new Circle(0.0, 1.0, 2.0);
-        String circleString = NoHibernateUtils.object2CustomString(circleObject, objectMapper);
+        String circleString = NoHibernateUtils.object2CustomString(circleObject);
         String expectedString = "{\"class\":\"cz.pedry.nohibernate.model.Circle\",\"data\":{\"x\":0.0,\"y\":1.0,\"r\":2.0}}";
         Assert.assertEquals(expectedString, circleString);
     }
 
     @Test
     public void simplePojoShouldBeDeserializedProperly() throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
-        Object circleObject = new Circle(0.0, 1.0, 2.0);
         String circleString = "{\"class\":\"cz.pedry.nohibernate.model.Circle\",\"data\":{\"x\":0.0,\"y\":1.0,\"r\":2.0}}";
-        Object expectedObject = NoHibernateUtils.customString2Object(circleString, objectMapper);
-        Assert.assertEquals(circleObject.toString(), expectedObject.toString());
+        Object circleObject = NoHibernateUtils.customString2Object(circleString);
+        Object expectedObject = new Circle(0.0, 1.0, 2.0);
+        Assert.assertEquals(expectedObject.toString(), circleObject.toString());
     }
 
 }
