a6c59acc251e4ba92b1f053482fb7f3e6b8c3aa5
ppedrycz <piotrekpedrycz@gmail.com>
2017-04-23 14:26:41 +0200

    Switched to maven

--- src/test/java/cz/pedry/nohibernate/SerializationTest.java
+++ src/test/java/cz/pedry/nohibernate/SerializationTest.java
@@ -13,7 +13,7 @@ public class SerializationTest {
         ObjectMapper objectMapper = new ObjectMapper();
         Object circleObject = new Circle(0.0, 1.0, 2.0);
         String circleString = NoHibernateUtils.object2CustomString(circleObject, objectMapper);
-        String expectedString = "{\"class\":\"Circle\",\"data\":{\"x\":0.0,\"y\":1.0,\"r\":2.0}}";
+        String expectedString = "{\"class\":\"cz.pedry.nohibernate.model.Circle\",\"data\":{\"x\":0.0,\"y\":1.0,\"r\":2.0}}";
         Assert.assertEquals(expectedString, circleString);
     }
 
@@ -21,7 +21,7 @@ public class SerializationTest {
     public void simplePojoShouldBeDeserializedProperly() throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
         Object circleObject = new Circle(0.0, 1.0, 2.0);
-        String circleString = "{\"class\":\"Circle\",\"data\":{\"x\":0.0,\"y\":1.0,\"r\":2.0}}";
+        String circleString = "{\"class\":\"cz.pedry.nohibernate.model.Circle\",\"data\":{\"x\":0.0,\"y\":1.0,\"r\":2.0}}";
         Object expectedObject = NoHibernateUtils.customString2Object(circleString, objectMapper);
         Assert.assertEquals(circleObject.toString(), expectedObject.toString());
     }
